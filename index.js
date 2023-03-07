a647bcdb4b967c6d53ce6baefc2f589b9722b536
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-06 21:51:36 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+One of your coworkers at the tech company you work at has made a huge error - they have filled up the company's database with strings that are written with dashes and underscores separating each word. That wouldn't be a big deal, except for the fact that your company's entire codebase relies upon the fact that every piece of string data in the company database is written in camel case!
+
+Write a function that takes in a string (you will see examples of it below) and converts it into camel case. 
+
+Note: Camel case is where the first letter of the first word is lowercased, while every subsequent first letter of each other word is uppercased. 
+
+For example,
+If your function was given an input like "hello-world-variable",
+your function should output "helloWorldVariable"
+
+If your function was given an input like "all cows eat grass", 
+your function should output "allCowsEatGrass"
+
+If your function was given an input like "please_excuse_my_dear_aunt_sally",
+your function should output "pleaseExcuseMyDearAuntSally"
+
+Note 2: You can always expect that your string input will contain at least 2 words.
+Note 3: You can always expect that the words in your string will either be separated by dashes, spaces, or underscores. 
+
+*/
+
+// Write your code below: 
